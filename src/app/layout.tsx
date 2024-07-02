7190e11b11af190f2e33493ad7fc91063c8fc62c
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2024-07-02 13:28:27 +0200

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/wordart.png" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>{children}</body>
     </html>
