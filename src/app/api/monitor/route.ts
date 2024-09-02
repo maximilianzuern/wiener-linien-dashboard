5bf0eb9f94899758447885c305f2f289171704c2
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2024-09-02 13:10:46 +0200

    moved parseData to route.ts, added Callback, split components

--- src/app/api/monitor/route.ts
+++ src/app/api/monitor/route.ts
@@ -1,37 +1,41 @@
 import type { NextRequest } from "next/server";
-// import { getRequestContext } from "@cloudflare/next-on-pages";
-// const { env, cf, ctx } = getRequestContext();
+import { parseData } from "./utils";
+import { Welcome } from "@/app/types";
 
 export const runtime = "edge";
 
-const API_BASE_URL = "https://www.wienerlinien.at/ogd_realtime/monitor";
+const API_BASE_URL: string = "https://www.wienerlinien.at/ogd_realtime/monitor";
+const MAX_COUNTDOWN: number = 40;
 
 export async function GET(request: NextRequest) {
-  try {
-    const params = request.nextUrl.searchParams.getAll("stopID");
-    const query = params.map((id) => `stopId=${id}`).join("&");
+  const params = request.nextUrl.searchParams.getAll("stopID");
+  const query = new URLSearchParams(params.map(id => ["stopId", id])).toString();
 
+  try {
     const response = await fetch(`${API_BASE_URL}?${query}`, {
-      method: "GET",
       headers: { "Accept-Language": "de" },
     });
 
-    // checking !response.ok in page.tsx
+    if (!response.ok) {
+      throw new Error(`Wiener Linien API request failed with status ${response.status}`);
+    }
 
-    // if (!response.ok) {
-    //   return new Response(JSON.stringify({ error: "Fetch failed" }), {
-    //     status: response.status,
-    //     statusText: response.statusText,
-    //     headers: response.headers,
-    //   });
-    // }
+    const data: Welcome = await response.json();
 
-    return new Response(response.body, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: response.headers,
-    });
+    if (data.data?.monitors.length === 0 || data.message.value !== "OK") {
+      throw new Error(data.data?.monitors.length === 0 && data.message.value === "OK" ? "No departures found for the given stopID." : `API response: ${data.message.value}`);
+    }
+
+    let parsedData;
+    try {
+      parsedData = parseData(data, MAX_COUNTDOWN);
+    } catch (error) {
+      throw new Error("Real time data not available. 😓🚨");
+    }
+
+    return Response.json(parsedData);
   } catch (error) {
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
+    const errorMessage = error instanceof Error ? error.message : "Internal Server Error";
+    return Response.json({ error: errorMessage }, { status: 500 });
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { useEffect, useState } from "react";
-import type { Welcome, OutputData } from "./types";
-import { useSearchParams } from "next/navigation";
 
-const DEFAULT_STOP_IDS = [4111, 4118];
-const API_BASE_URL = "/api/monitor";
-const MAX_COUNTDOWN = 40;
-const MAX_DISPLAYED_COUNTDOWNS = 6;
-const AIRCONDITIONED_METROS = ["U6"];
+import { useCallback, useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import type { OutputData } from "./types";
+
+const DEFAULT_STOP_IDS: number[] = [4111, 4118];
+const API_BASE_URL: string = "/api/monitor";
+const MAX_DISPLAYED_COUNTDOWNS: number = 6;
+const AIRCONDITIONED_METROS: string[] = ["U6"];
 const TRANSPORT_EMOJI_LOOKUP: Record<string, string> = {
   ptTram: "🚃",
   ptTramWLB: "🚃",
@@ -17,164 +17,72 @@ const TRANSPORT_EMOJI_LOOKUP: Record<string, string> = {
   ptTrainS: "🚆",
 };
 
+type FetchResult = Record<string, OutputData[]> | { error: string };
+
 // to get the stopID -> https://till.mabe.at/rbl/
-async function fetchData(stopIDs: number[] = [], invalidKey: boolean): Promise<Welcome | { error: string }> {
-  const validStopIDs = stopIDs.length > 0 ? stopIDs : DEFAULT_STOP_IDS;
-  const query = new URLSearchParams(validStopIDs.map((id) => ["stopID", id.toString()])).toString();
+async function fetchData(stopIDs: number[] = [], invalidKey: boolean): Promise<FetchResult> {
+// async function fetchData(stopIDs: number[]): Promise<FetchResult> {
+  const query = new URLSearchParams(stopIDs.map(id => ["stopID", id.toString()])).toString();
   if (invalidKey) {
     return {error: "Invalid stopID key in URL."};
   }
   
   try {
     const res = await fetch(`${API_BASE_URL}?${query}`);
-    if (!res.ok) {
-      throw new Error(`Wiener Linien API request failed with status ${res.status}`);
-    }
-
-    const data: Welcome = await res.json();
-
-    if (data.data?.monitors.length === 0 && data.message.value === "OK") {
-      return {
-        error: "No departures found for the given stopID.",
-      };
-    } else if (data.message.value !== "OK") {
-      return {
-        error: `API response: ${data.message.value}`,
-      };
-    }
-
-    return data;
+    return await res.json();
   } catch (error) {
-    return {
-      error: error instanceof Error ? `Error: ${error.message}` : "An unknown error occurred.",
-    };
+    return { error: error instanceof Error ? error.message : "An unknown error occurred." };
   }
 }
 
-function parseData(data: Welcome): Record<string, OutputData[]> {
-  const result: Record<string, OutputData[]> = {};
-
-  data.data.monitors.forEach((monitor) => {
-    const title = monitor.locationStop.properties.title;
-
-    monitor.lines.forEach((line) => {
-      const { name, towards, type, departures } = line;
-      const countdowns =
-        departures?.departure
-          .map((departure) => departure.departureTime.countdown)
-          .filter((countdown) => countdown <= MAX_COUNTDOWN) ?? [];
-      const timeReal =
-        departures?.departure.map((departure) => {
-          const date = new Date(departure.departureTime?.timeReal ?? "");
-          return date.toTimeString().split(" ")[0];
-        }) ?? [];
-      const timePlanned = departures?.departure.map((departure) => {
-        const date = new Date(departure.departureTime.timePlanned);
-        return date.toTimeString().split(" ")[0];
-      });
-      const aircon =
-        departures?.departure.map((dep, index) =>
-          index < 2 ? dep.vehicle?.foldingRampType !== undefined : undefined
-        ) ?? [];
-
-      if (!result[title]) {
-        result[title] = [];
-      }
-      result[title].push({ name, towards, type, countdowns, timeReal, timePlanned, aircon });
-    });
-  });
-  return Object.fromEntries(Object.entries(result).sort());
-}
-
 export default function Home() {
-  const [error, setError] = useState<string | null>(null);
-  const [parsedData, setParsedData] = useState<Record<string, OutputData[]>>();
+  const [fetchedError, setError] = useState<string | OutputData[] | null>(null);
+  const [data, setData] = useState<FetchResult | null>(null);
 
   const searchParams = useSearchParams();
-  // validate the searchParams
-  const keys = Array.from(searchParams.keys());
-  const invalidKey = keys.some((key) => key.toLowerCase() !== "stopid");
+  const getStopIDs = useCallback(() => {
+    const params = searchParams.getAll("stopID");
+    return params.length > 0 ? params.map(Number) : DEFAULT_STOP_IDS;
+  }, [searchParams]);
 
+  // validate the searchParams
+  // const keys = Array.from(searchParams.keys());
+  // const invalidKey = keys.some((key) => key.toLowerCase() !== "stopid");
+  const invalidKey = Array.from(searchParams.keys()).some(key => key.toLowerCase() !== "stopid");
   const query = Array.from(searchParams.values()).map(Number);
 
   useEffect(() => {
-    fetchData(query, invalidKey).then((result) => {
-      if ("error" in result) {
+    const stopIDs = getStopIDs();
+    fetchData(stopIDs, invalidKey).then((result) => {
+      if (result.error) {
         setError(result.error);
       } else {
-        try {
-          setParsedData(parseData(result));
-        } catch (e) {
-          setError("Real time data not available. 😓🚨");
-        }
+        setData(result);
       }
     });
   }, []);
 
+  // useEffect(() => {
+  //   if (!invalidKey) {
+  //     fetchData(getStopIDs()).then(setData);
+  //   }
+  // }, [getStopIDs, invalidKey]);
+
   if (invalidKey) {
-    return (
-      <div className="container mx-auto p-2">
-        <h1 className="text-xl font-bold text-center mb-2">Vienna Public Transport</h1>
-        <div className="text-center text-red-500 font-semibold my-10">
-          Error in URL: {searchParams.toString()}
-        </div>
-        <div className="text-center text-red-500 font-semibold my-10">
-          Please provide a valid stopID.
-        </div>
-        <Footer />
-      </div>
-    );
+  return <ErrorMessage message="Invalid stopID key in URL." />;
   }
 
-  const noParamsMessage =
-    query.length === 0 ? (
-      <div
-        className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4"
-        role="alert"
-      >
-        <p className="font-bold">Using default stops</p>
-        <p>To see departures for specific stops:</p>
-        <ol className="list-decimal list-inside mt-1">
-          <li>
-            Find valid stop IDs{" "}
-            <a
-              href="https://till.mabe.at/rbl/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 underline"
-            >
-              here
-            </a>
-          </li>
-          <li>
-            Add to the web address:{" "}
-            <span className="font-mono bg-yellow-200 px-1">/?stopID=123</span>
-          </li>
-          <li>
-            For multiple stops, use:{" "}
-            <span className="font-mono bg-yellow-200 px-1">/?stopID=123&amp;stopID=456</span>
-          </li>
-        </ol>
-      </div>
-    ) : null;
-
-  if (error) {
-    return (
-      <div className="container mx-auto p-2">
-        <h1 className="text-xl font-bold text-center mb-2">Vienna Public Transport</h1>
-        <div className="text-center text-red-500 font-semibold my-10">{error}</div>
-        <Footer />
-      </div>
-    );
+  if (fetchedError) {
+    return <ErrorMessage message={fetchedError.toString()} />;
   }
 
   return (
     <div className="container mx-auto p-2">
       <h1 className="text-xl font-bold text-center mb-1">Vienna Public Transport</h1>
-      {noParamsMessage}
+      {query.length === 0 && <DefaultStopsMessage />}
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
-        {parsedData &&
-          Object.entries(parsedData).map(([title, lines]) => (
+        {data &&
+          Object.entries(data).map(([title, lines]) => (
             <StopCard key={title} title={title} lines={lines} />
           ))}
       </div>
@@ -203,11 +111,7 @@ const LineInfo = ({ line }: { line: OutputData }) => (
         {line.name} {TRANSPORT_EMOJI_LOOKUP[line.type as keyof typeof TRANSPORT_EMOJI_LOOKUP] ?? ""}
       </div>
       <div className="text-gray-500">
-        {line.towards.includes(" ")
-          ? line.towards.charAt(0) +
-            line.towards.slice(1).toLowerCase().split(" ")[0] +
-            line.towards.slice(line.towards.indexOf(" "))
-          : line.towards.charAt(0).toUpperCase() + line.towards.slice(1).toLowerCase()}
+          {formatTowards(line.towards)}
       </div>
     </div>
     <div className="ml-3 mt-5">
@@ -278,7 +182,7 @@ const CountdownBadge = ({
         </div>
       )}
       {type === "ptMetro" &&
-        hasAircon !== undefined &&
+        hasAircon !== null &&
         (hasAircon ? (
           <span className="absolute -top-2 -right-1 text-xs" title="❄️ A/C available">
             ❄️
@@ -305,8 +209,35 @@ const Footer = () => (
     </a>
     .
     <br />
-    e.g &apos;/?stopID=123&amp;stopID=124&apos; to specify stopIDs.
+    e.g &apos;/?stopID=4111&amp;stopID=4118&apos; to specify stopIDs.
     <br />
     🍪 This website is cookie-free.
   </div>
 );
+
+const ErrorMessage = ({ message }: { message: string }) => (
+  <div className="container mx-auto p-2">
+    <h1 className="text-xl font-bold text-center mb-2">Vienna Public Transport</h1>
+    <div className="text-center text-red-500 font-semibold my-10">{message}</div>
+    <Footer />
+  </div>
+);
+
+const DefaultStopsMessage = () => (
+  <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4" role="alert">
+    <p className="font-bold">Using default stops</p>
+    <p>To see departures for specific stops:</p>
+    <ol className="list-decimal list-inside mt-1">
+      <li>Find valid stop IDs <a href="https://till.mabe.at/rbl/" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">here</a></li>
+      <li>Add to the web address: <span className="font-mono bg-yellow-200 px-1">/?stopID=4111</span></li>
+      <li>For multiple stops, use: <span className="font-mono bg-yellow-200 px-1">/?stopID=4111&amp;stopID=4118</span></li>
+    </ol>
+  </div>
+);
+
+const formatTowards = (towards: string) => {
+  const words = towards.toLowerCase().split(" ");
+  return words.length > 1
+    ? words[0].charAt(0).toUpperCase() + words[0].slice(1) + " " + words.slice(1).join(" ")
+    : towards.charAt(0).toUpperCase() + towards.slice(1).toLowerCase();
+};
