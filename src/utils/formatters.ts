d9441a0e34c697f07b1c020c837ad6b70e9bd347
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2024-09-05 22:02:01 +0200

    fixed formatter

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,8 +1,8 @@
 export const formatTowards = (towards: string): string => {
-  const words = towards.split(" ");
+  const words = towards.split(/[\s-]/);
   if (words.length > 1) {
     const firstWord = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
-    const restOfString = towards.slice(towards.indexOf(" "));
+    const restOfString = towards.slice(towards.indexOf(words[0]) + words[0].length);
     return firstWord + restOfString;
   } else {
     return towards.charAt(0).toUpperCase() + towards.slice(1).toLowerCase();
