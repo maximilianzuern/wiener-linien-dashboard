6efe29301eb3cdacedc34b3554a4db0481ad666d
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2024-09-08 02:09:14 +0200

    fixed dot formatting

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,5 +1,5 @@
 export const formatTowards = (towards: string): string => {
-  const words = towards.split(/[\s-]/);
+  const words = towards.split(/[\s-.]/);
   if (words.length > 1) {
     const firstWord = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
     const restOfString = towards.slice(towards.indexOf(words[0]) + words[0].length);
