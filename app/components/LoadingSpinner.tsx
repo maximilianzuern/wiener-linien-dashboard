43b223f8cfdcf9c3c7762a19d0df5e5d4a7c21be
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2026-06-27 23:15:38 +0200

    npm run fmt

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -1,23 +1,23 @@
 import { SkeletonLine, LayerCard, Grid, GridItem } from "@cloudflare/kumo";
 
 export const LoadingSpinner = () => (
-    <Grid variant="3up" gap="sm">
-      {/* show skeleton loading states three times */}
-      {[0, 1, 2].map((item) => (
-        <GridItem key={item}>
-          <LayerCard>
-            <LayerCard.Secondary>
-              <SkeletonLine className="h-4" minWidth={16} maxWidth={32} />
-            </LayerCard.Secondary>
+  <Grid variant="3up" gap="sm">
+    {/* show skeleton loading states three times */}
+    {[0, 1, 2].map((item) => (
+      <GridItem key={item}>
+        <LayerCard>
+          <LayerCard.Secondary>
+            <SkeletonLine className="h-4" minWidth={16} maxWidth={32} />
+          </LayerCard.Secondary>
 
-            <LayerCard.Primary>
-              <div className="space-y-3">
-                <SkeletonLine className="h-4" blockHeight={32} minWidth={16} maxWidth={32} />
-                <SkeletonLine className="h-4" blockHeight={32} minWidth={48} maxWidth={64} />
-              </div>
-            </LayerCard.Primary>
-          </LayerCard>
-        </GridItem>
-      ))}
-    </Grid>
+          <LayerCard.Primary>
+            <div className="space-y-3">
+              <SkeletonLine className="h-4" blockHeight={32} minWidth={16} maxWidth={32} />
+              <SkeletonLine className="h-4" blockHeight={32} minWidth={48} maxWidth={64} />
+            </div>
+          </LayerCard.Primary>
+        </LayerCard>
+      </GridItem>
+    ))}
+  </Grid>
 );
