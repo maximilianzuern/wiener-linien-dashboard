a8a701f74d44d410c14ce6142075a3e985ca63fb
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2026-06-28 13:11:50 +0200

    Fix DOM issue: stroke-width attribute to strokeWidth

--- app/components/CountdownBadge.tsx
+++ app/components/CountdownBadge.tsx
@@ -56,9 +56,9 @@ const CountdownBadge = ({
           title={hasAircon ? "❄️ A/C available" : "🥵 No A/C"}
         >
           {hasAircon ? (
-            <Snowflake className="h-4 w-4 text-blue-500" stroke-width="2" />
+            <Snowflake className="h-4 w-4 text-blue-500" strokeWidth="2" />
           ) : (
-            <Flame className="h-4 w-4 text-orange-600" stroke-width="3" />
+            <Flame className="h-4 w-4 text-orange-600" strokeWidth="3" />
           )}
         </span>
       )}
