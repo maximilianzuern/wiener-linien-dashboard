ac7cfdcd9c7e55ce5b2a8415cd7aff4bfdc3cce0
Maximilian <85677120+maximilianzuern@users.noreply.github.com>
2026-07-28 00:04:28 +0200

    remove vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,10 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
-    tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [cloudflare({ viteEnvironment: { name: "ssr" } }), tailwindcss(), reactRouter()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
